9e2835e18f8324d8bdba107f19cfa083a1722ac6
Hebert Fernandes <hebertdelima13@gmail.com>
2022-01-13 17:31:25 -0300

    projeto finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'overview', component: OverviewComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SwiperModule } from 'swiper/angular';
-import { HomeComponent } from './pages/home/home.component';
 import { NgChartsModule } from 'ng2-charts';
 import { SendMoneySliderComponent } from './components/send-money-slider/send-money-slider.component';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SendMoneySliderComponent
+    SendMoneySliderComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SwiperModule,
-    NgChartsModule
+    NgChartsModule,
+    ReactiveFormsModule,
+    SweetAlert2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
